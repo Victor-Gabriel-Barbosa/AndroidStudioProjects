2a0ad28c81d5fcaf613e3a72a7048030dc555dd5
Victor Gabriel <167694976+Victor-Gabriel-Barbosa@users.noreply.github.com>
2025-11-08 21:03:01 -0300

    feat: Att

--- VideoCompose/app/src/main/java/com/example/videocompose/MainActivity.kt
+++ VideoCompose/app/src/main/java/com/example/videocompose/MainActivity.kt
@@ -1,43 +1,23 @@
 package com.example.videocompose
 
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.net.toUri
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
-import com.example.videocompose.ui.theme.VideoComposeTheme
-import androidx.core.net.toUri
 import androidx.media3.ui.PlayerView
+import com.example.videocompose.ui.theme.VideoComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,9 +26,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             VideoComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Video(
-                        videoUri = "https://www.w3schools.com/html/mov_bbb.mp4"
-                    )
+                    Box(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    ) {
+                        Video(
+                            videoUri = "https://www.w3schools.com/html/mov_bbb.mp4"
+                        )
+                    }
                 }
             }
         }
